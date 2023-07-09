3450e81757ac7811185a79860420946cd3fe8cb8
SAI TEJA H R <98202897+Saitejahr@users.noreply.github.com>
2023-07-09 10:52:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let arr = [1, 2, 3, 4, 5];
 function incrementArray(arr) {
   let newArr = [];
-	for(let i=0;i<n;i++){
+	for(let i=0;i< arr.length;i++){
 		newArr[i] = ++arr[i];
 	}
 	return newArr;
